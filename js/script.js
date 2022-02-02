a378dcbfd040d2f4d551f21afb533ad6dfe9b881
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2022-02-02 12:30:23 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
-let user = {
-    data: {
-        a: 1,
-        b: 2,
-        c: 3,
-        d: {
-            a1: 1,
-            b1: 2,
-            c1: 3,
-            d1: {
-                a2: 3,
-                b2: 3,
-                c2: 3,
-            }
-        },
+"use strict";
+(function() {
+    let user = {
+        data: {
+            a: 1,
+            b: 2,
+            c: 3,
+            d: {
+                a1: 1,
+                b1: 2,
+                c1: 3,
+                d1: {
+                    a2: 3,
+                    b2: 3,
+                    c2: 3,
+                }
+            },
+        }
     }
-}
-
-Object.freeze(user);
-
-user.data = "SomeData";
-console.log(user.data);
-function DeepFreeze(arg) {
     
-}
+
+DeepFreeze(user);
+user['a'] = "value3";
+
+
+    function DeepFreeze(obj) {
+        getObjData(obj);
+        
+        function getObjData(o) {
+
+            for (let prop in o) {
+                if(typeof o[prop] === "object") {
+                    getObjData(o[prop])
+                } else {
+                    console.log('object value:', o[prop]);
+                    writable:false;
+                    configurable:false;
+                }
+            }
+        }
+    }
+console.log(Object.entries(user));
+})();
